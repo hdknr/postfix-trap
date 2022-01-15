a239c9785184844fb933933077967ce010dff501
user.name <gmail@hdknr.com>
2022-01-15 18:18:36 +0900

    trap works

--- docker/postfix/bin/entrypoint.sh
+++ docker/postfix/bin/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # https://zenn.dev/flyingbarbarian/articles/5bb1d38b1ada40
 
-/etc/init.d/postfix start
+/etc/init.d/postfix stop
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
-/etc/init.d/postfix start
+/etc/init.d/saslauthd restart
 
 # keep container running
-tail -f /dev/null
+# tail -f /dev/null
+/usr/sbin/postfix start-fg
--- docker/postfix/bin/setup_postfix.sh
+++ docker/postfix/bin/setup_postfix.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 apt update 
-apt-get install postfix -y
+apt-get install postfix libsasl2-modules sasl2-bin -y
+apt-get install mailutils lsof net-tools vim -y
+adduser postfix sasl
+#
+mkdir -p /var/spool/postfix/var/run/saslauthd
